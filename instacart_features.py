b7a4ae2a8b56f7330af06ba20db98188efa5a821
Om Shinde <om.shinde@caa.iitgn.ac.in>
2026-04-19 16:44:17 +0530

    Create user-level and user-product interaction features

--- instacart_features.py
+++ instacart_features.py
@@ -5,7 +5,7 @@ import lightgbm as lgb
 IDIR = '../input/'
  
 
-# SECTION 1: Load Data
+#Load Data
 
  
 print('loading prior')
@@ -46,7 +46,7 @@ print('train {}: {}'.format(train.shape, ', '.join(train.columns)))
  
  
 
-# SECTION 2: Product-level Features
+#Product-level Features
 
  
 print('computing product features')
@@ -62,3 +62,51 @@ print('add order info to priors')
 orders.set_index('order_id', inplace=True, drop=False)
 priors = priors.join(orders, on='order_id', rsuffix='_')
 priors.drop('order_id_', inplace=True, axis=1)
+
+
+
+# User-level Features
+
+print('computing user features')
+usr = pd.DataFrame()
+usr['average_days_between_orders'] = orders.groupby('user_id')['days_since_prior_order'].mean().astype(np.float32)
+usr['nb_orders'] = orders.groupby('user_id').size().astype(np.int16)
+ 
+users = pd.DataFrame()
+users['total_items'] = priors.groupby('user_id').size().astype(np.int16)
+users['all_products'] = priors.groupby('user_id')['product_id'].apply(set)
+users['total_distinct_items'] = (users.all_products.map(len)).astype(np.int16)
+ 
+users = users.join(usr)
+del usr
+users['average_basket'] = (users.total_items / users.nb_orders).astype(np.float32)
+print('user features shape:', users.shape)
+
+
+
+
+#User x Product Features (original)
+
+print('computing user x product features...')
+priors['user_product'] = priors.product_id + priors.user_id * 100000
+ 
+d = dict()
+for row in priors.itertuples():
+    z = row.user_product
+    if z not in d:
+        d[z] = (1,
+                (row.order_number, row.order_id),
+                row.add_to_cart_order)
+    else:
+        d[z] = (d[z][0] + 1,
+                max(d[z][1], (row.order_number, row.order_id)),
+                d[z][2] + row.add_to_cart_order)
+ 
+print('converting to dataframe...')
+userXproduct = pd.DataFrame.from_dict(d, orient='index')
+del d
+userXproduct.columns = ['nb_orders', 'last_order_id', 'sum_pos_in_cart']
+userXproduct.nb_orders = userXproduct.nb_orders.astype(np.int16)
+userXproduct.last_order_id = userXproduct.last_order_id.map(lambda x: x[1]).astype(np.int32)
+userXproduct.sum_pos_in_cart = userXproduct.sum_pos_in_cart.astype(np.int16)
+print('user x product features count:', len(userXproduct))
